83b7efcf767797a15649b0796610d5727eb19cec
Quốc Long Phan <phqlong2810@gmail.com>
2021-05-05 16:01:21 +0700

    Add Footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -6,11 +6,30 @@ import { useDispatch, useSelector } from "react-redux"
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {Nav, Navbar, NavDropdown, FormControl, Button } from 'react-bootstrap';
 
+import {FaFacebook, FaYoutube, FaInstagram} from "react-icons/fa"
+
+import "./Footer.css"
+
 const Footer = () => {
     return (
-        <>
-        </>
+        <section className="Footer">
+            <Row>
+                <Col>
+                    <h1 className="Footer__brand"><a href="/home">BKSOFT</a></h1>
+                    <h6><a href="/contact">Contact us</a></h6>
+                    <h6><a href="/aboutus">About us</a></h6>
+                </Col>
 
+                <Col className="Footer__icons">
+                    <a href="#" className="icon"> <FaFacebook size="30px" /> </a>
+                    <a href="#" className="icon"> <FaYoutube size="30px" /> </a>
+                    <a href="#" className="icon"> <FaInstagram size="30px" /> </a>
+                </Col>
+            </Row>
+
+            <h6 className="Footer__copyright"> Copyright © 2021-2031 <a href="/home">BKSOFT</a> | All rights reserved</h6>
+
+        </section>
     )
 }
 

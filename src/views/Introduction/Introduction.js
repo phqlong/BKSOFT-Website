2398f4af911037686e47148aae696079299aa382
dungmeo47 <dungmeo47@gmail.com>
2021-05-06 09:09:05 +0700

    add news

--- src/views/Introduction/Introduction.js
+++ src/views/Introduction/Introduction.js
@@ -1,16 +1,29 @@
 import { useState } from "react"
 import { Link, Redirect, useHistory } from 'react-router-dom'
-import { Form ,Col, Dropdown, DropdownItem, DropdownMenu, DropdownToggle, FormGroup, Input, InputGroup, InputGroupAddon, List, Row } from "reactstrap"
+import { Form, Col, Dropdown, DropdownItem, DropdownMenu, DropdownToggle, FormGroup, Input, InputGroup, InputGroupAddon, List, Row, Container } from "reactstrap"
 import { useDispatch, useSelector } from "react-redux"
 
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {Nav, Navbar, NavDropdown, FormControl, Button } from 'react-bootstrap';
+import {Nav, Navbar, NavDropdown, FormControl, Button, Carousel } from 'react-bootstrap';
 
+import './Introduction.css'   
 const Introduction = () => {
     return (
-        <>
-        <p>Hello World</p>
-        </>
+        <Container>
+            <section class="s-aboutus">
+                <h2>Chúng tôi là BKSoft</h2>
+                <p>Thành lập ngày 6/9/2013, BKSoft hiện là một trong những công ty internet và dịch vụ công nghệ lớn nhất Việt Nam.</p>
+            </section>
+
+            <section class="s-products bg-carrot">
+
+            </section>
+
+            <section class="s-history bg-light ">
+
+            </section>
+        </Container>
+
 
     )
 }
--- src/views/News/News/News.js
+++ src/views/News/News/News.js
@@ -1,15 +1,111 @@
 import { useState } from "react"
 import { Link, Redirect, useHistory } from 'react-router-dom'
-import { Form ,Col, Dropdown, DropdownItem, DropdownMenu, DropdownToggle, FormGroup, Input, InputGroup, InputGroupAddon, List, Row } from "reactstrap"
+import { Form, Col, Dropdown, DropdownItem, DropdownMenu, DropdownToggle, FormGroup, Input, InputGroup, InputGroupAddon, List, Row } from "reactstrap"
 import { useDispatch, useSelector } from "react-redux"
 
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {Nav, Navbar, NavDropdown, FormControl, Button } from 'react-bootstrap';
+import { Nav, Navbar, NavDropdown, FormControl, Button, Container, Carousel, Card } from 'react-bootstrap';
+
+import "./news.css"
+import new1 from "../../../assets/img/new1.jpg"
+import new2 from "../../../assets/img/new2.jpg"
+import new3 from "../../../assets/img/new3.jpg"
 
 const News = () => {
+    const [index, setIndex] = useState(0);
+
+    const handleSelect = (selectedIndex, e) => {
+        setIndex(selectedIndex);
+    };
     return (
-        <>
-        </>
+        <Container>
+            <Carousel activeIndex={index} onSelect={handleSelect}>
+                <Carousel.Item>
+                    <img
+                        className="d-block w-100"
+                        src={new1}
+                        alt="First slide"
+                    />
+                </Carousel.Item>
+
+                <Carousel.Item>
+                    <img
+                        className="d-block w-100"
+                        src={new2}
+                        alt="Second slide"
+                    />
+                </Carousel.Item>
+
+                <Carousel.Item>
+                    <img
+                        className="d-block w-100"
+                        src={new3}
+                        alt="Third slide"
+                    />
+                </Carousel.Item>
+            </Carousel>
+
+            <Row>
+                <Col lg={true}>
+                    <Card>
+                        <Card.Img variant="top" src={new1} />
+                        <Card.Body>
+                            <Card.Title>
+                                Minathon 2021
+                            </Card.Title>
+                            <Card.Text>
+                                BKSoft là đơn vị đồng hành cùng cuộc thi sáng tạo giải pháp công nghệ Minathon 2021...
+                            </Card.Text>
+                            <div className="text-muted">
+                                20-03-2021
+                            </div>
+                        </Card.Body>
+                        <Card.Footer>
+                            <a href="https://www.facebook.com/BKCSE.Multimedia/posts/4347451018615342/">Xem thêm</a>
+                        </Card.Footer>
+                    </Card>
+                    <br />
+                </Col>
+
+                <Col lg={true}>
+                    <Card>
+                        <Card.Img variant="top" src={new2} />
+                        <Card.Body>
+                            <Card.Title>Top 5 game hay nhất</Card.Title>
+                            <Card.Text>
+                                Theo thống kê của hiệp hội thể thao điện tử Việt Nam (VRESA), tựa game Tree Story lọt top 5...
+                            </Card.Text>
+                            <div className="text-muted">
+                                25-02-2021
+                            </div>
+                        </Card.Body>
+                        <Card.Footer>
+                            <a href="http://viresa.org.vn/">Xem thêm</a>
+                        </Card.Footer>
+                    </Card>
+                    <br />
+                </Col>
+
+                <Col lg={true}>
+                    <Card>
+                        <Card.Img variant="top" src={new3} />
+                        <Card.Body>
+                            <Card.Title>Tăng trưởng vượt bậc</Card.Title>
+                            <Card.Text>
+                                BKSoft vừa công bố doanh thu thuần năm 2020 đạt hơn 1.000 tỷ VND, tăng gần 14% so với...
+                            </Card.Text>
+                            <div className="text-muted">
+                                23-01-2021
+                            </div>
+                        </Card.Body>
+                        <Card.Footer>
+                            <a href="https://www.vng.com.vn/news/news/vng-cong-bo-dau-tu-manh-cho-startup.html">Xem thêm</a>
+                        </Card.Footer>
+                    </Card>
+                    <br />
+                </Col>
+            </Row>
+        </Container>
 
     )
 }
